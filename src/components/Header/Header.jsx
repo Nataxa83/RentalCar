3fab6eb97509f88eda6e7995f67d09d0c5ecd4cb
Nataliia Hordiienko <Natalihordienko@gmail.com>
2025-02-27 23:03:26 +0100

    fixed favicon on vercel

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import Logo from "../Logo/Logo"
 import Navigation from "../Navigation/Navigation"
+import css from "./Header.module.css"
 
 const Header = () => {
-    // const iconLogo = "icon-logo";
   return (
-    <div>
+    <header className={css.container}>
+    <div className={css.header}>
         <Logo />
         <Navigation />
     </div>
+    </header>
   )
 }
 
