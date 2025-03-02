aef19ed44b04ab7654aa7aacfbf06e75b44e4b6a
Nataliia Hordiienko <Natalihordienko@gmail.com>
2025-03-02 19:26:35 +0100

    fixed rendering catalogPage

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,8 @@
-// import AppBar from "../AppBar/AppBar"
-import { useSelector } from "react-redux";
-import CatalogList from "../CatalogList/CatalogList"
 import css from "./Layout.module.css"
 
 const Layout = ({children}) => {
-  const cars = useSelector(state => state.carsData.cars);
   return (
     <div className={css.container}>
-        <CatalogList cars= {cars}/>
         {children}
     </div>
   )
